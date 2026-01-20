51e47e587a5a8c9624fba87b25940ce8ce9b8599
Prahaladh <prahaladh05@example.com>
2026-01-20 19:26:20 +0530

    yooo

--- test2.js
+++ test2.js
@@ -31,6 +31,7 @@ function withRetry(asyncFn, retries = 3, delayMs = 200) {
   const b = 20;
   console.log(a + b);
   console.log(a - b);
+  console.log(a ** b);
   if (typeof asyncFn !== "function") {
     throw new Error("asyncFn must be a function");
   }
